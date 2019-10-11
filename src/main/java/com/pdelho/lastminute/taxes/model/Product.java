dd513acc3594ce5929623326f956e72c5b556a88
pablo <pablodelhoyo@gmail.com>
2019-10-11 20:19:36 +0200

    Corerect positive integer validation

--- src/main/java/com/pdelho/lastminute/taxes/model/Product.java
+++ src/main/java/com/pdelho/lastminute/taxes/model/Product.java
@@ -1,8 +1,8 @@
 package com.pdelho.lastminute.taxes.model;
 
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.NotBlank;
 
@@ -12,7 +12,7 @@ public class Product {
 	
 	// Positive number
 	@NotNull(message = "Enter a quantity")
-	@Pattern(regexp = "[\\s]*[0-9]*[1-9]+",message="Enter a positive integer number")
+	@Digits(integer=10, fraction=0 ,message="Enter a positive integer number")
 	private String quantity;
 	
 	@NotBlank(message = "Include a description")
